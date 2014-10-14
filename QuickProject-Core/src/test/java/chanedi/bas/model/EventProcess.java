aecfbdc258bd17b5c520861b519a1daf44d58e0f
chanedi <chanedi@163.com>
2014-10-14 15:05:12 +0800

    QuickProject-10 sort参数的传入

--- QuickProject-Core/src/test/java/chanedi/bas/model/EventProcess.java
+++ QuickProject-Core/src/test/java/chanedi/bas/model/EventProcess.java
@@ -2,12 +2,14 @@ package chanedi.bas.model;
 
 import java.util.*;
 import lombok.Data;
+import javax.persistence.Table;
 import chanedi.model.Entity;
 
 @Data
+@Table(name = "T_EVE_EVENT_PROCESS")
 public class EventProcess extends Entity {
 
-    private static final long serialVersionUID = 4931040369800204073L;
+    private static final long serialVersionUID = -5227564951878481064L;
 
     private String modifyUserCode;
     private String processType;
--- QuickProject-Generator/src/test/resources/tmpl/java/model/${bean.capitalizeName}.ftl
+++ QuickProject-Generator/src/test/resources/tmpl/java/model/${bean.capitalizeName}.ftl
@@ -6,7 +6,7 @@ import javax.persistence.Table;
 import chanedi.model.Entity;
 
 @Data
-@Table(name = "bean.tableName")
+@Table(name = "${bean.tableName}")
 public class ${bean.capitalizeName} extends Entity {
 
     ${generate.serialVersionUID}
