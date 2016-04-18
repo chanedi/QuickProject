cb77b597d0496d1c642157da4270714a9035c5e2
chanedi <JIJINGYU625@lu.com>
2016-04-18 20:18:33 +0800

    code generator

---
+++ QuickProject-Generator/src/test/java/CodeGeneratorTest.java
@@ -0,0 +1,11 @@
+import chanedi.generator.exception.GlobalConfigException;
+
+/**
+ * @author Chanedi
+ */
+public class CodeGeneratorTest {
+
+    public static void main(String[] args) throws GlobalConfigException {
+    }
+
+}
